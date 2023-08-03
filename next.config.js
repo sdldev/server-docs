39c38d1f102e61a22204d1815133926d99e0756f
sdldev <sidolapakdev@gmail.com>
2023-08-04 01:26:36 +0700

    Change

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.jsx'
   })
-   
-  module.exports = 
-    withNextra({
-      output: 'standalone',
-
-    });
+  
+  module.exports =   withNextra();
+ 
