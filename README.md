56ab39b50237750c5fdb7c60849596b65c02d95a
sdldev <sidolapakdev@gmail.com>
2023-08-03 21:45:50 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# doc-pve
+
+SDL DEV Documentation
+Build with nextra + NextJs
---
+++ theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+    logo: <span>DOC PVE</span>,
+    project: {
+      link: 'https://github.com/sdldev/doc-pve',
+    },
+    // ...
+  }
